da65c3763c7a60287efb96917e4a288de779f58e
Art Balahadia <63167845+artbalahadia@users.noreply.github.com>
2020-06-09 00:16:07 -0500

    Add github config

---
+++ build/precache-manifest.38f1f9af5376d23586dbf78f0d0c4929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce79cde5ae826b052da1e9b47be03d7",
+    "url": "/imagery/index.html"
+  },
+  {
+    "revision": "f24f269ca2b8dcdc2b93",
+    "url": "/imagery/static/css/main.42b0a323.chunk.css"
+  },
+  {
+    "revision": "0568f3bdbf3ef6061774",
+    "url": "/imagery/static/js/2.b8d27544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imagery/static/js/2.b8d27544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f24f269ca2b8dcdc2b93",
+    "url": "/imagery/static/js/main.e5d0a737.chunk.js"
+  },
+  {
+    "revision": "4289ef7c86ae969bb5f3",
+    "url": "/imagery/static/js/runtime-main.a590aeda.js"
+  }
+]);
